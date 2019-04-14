cfc157d043eac31bc0b11f8854520abd01cbaeba
Lurns <lrnlee08@gmail.com>
2019-04-13 20:08:10 -0500

    Developed basic blog

---
+++ main.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:root@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(900))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+
+#workers
+def is_empty(val):
+    if val == "":
+        return True
+    else:
+        return False
+
+@app.route('/')
+def index():
+    return redirect('/blog')
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def new_post():
+
+    blogs = Blog.query.all()
+
+    if request.method == 'POST':
+        blog_title = request.form['blog_title']
+        blog_body = request.form['blog_body']
+
+        title_error = ''
+        body_error = ''
+
+        if is_empty(blog_title):
+            title_error = 'Please enter a title for your blog entry'
+        if is_empty(blog_body):
+            body_error = 'Please enter some words for your blog entry'
+
+        if not title_error and not body_error:
+            new_blog = Blog(blog_title, blog_body)
+            db.session.add(new_blog)
+            db.session.commit()
+
+            link = '/blog?id=' + str(new_blog.id)
+            return redirect(link)
+        
+        else:
+            return render_template('newpost.html',title='Build-A-Blog',
+            blogs=blogs,title_error=title_error,body_error=body_error,
+            blog_title=blog_title,blog_body=blog_body)
+    else:
+        return render_template('newpost.html',title='Build-A-Blog',blogs=blogs)
+
+@app.route('/blog')
+def entries():
+    entry_id = request.args.get("id")
+    blogs = Blog.query.all()
+
+    if entry_id == None:
+        return render_template('blog.html',title='Build-A-Blog',blogs=blogs)
+    else:
+        blog = Blog.query.filter_by(id=entry_id).one()
+        return render_template('entry.html',blog=blog, title=blog.title)
+
+if __name__ == '__main__':
+    app.run()
